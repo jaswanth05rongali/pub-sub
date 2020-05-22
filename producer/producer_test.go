a81ebc4a6661a6e8e734d3389b545cefce31c23e
bhaveshbaranda <bhaveshbaranda6@gmail.com>
2020-05-22 12:11:32 +0530

    Unit Tests 2

--- producer/producer_test.go
+++ producer/producer_test.go
@@ -1,19 +1,15 @@
-// package producer
+package producer
 
-// import "testing"
+import (
+	"testing"
+)
 
-// func TestProducerInit(t *testing.T) {
-// 	p, _ := Init("")
-// 	if p != nil {
-// 		t.Errorf("Init(\"\") FAILED, expected some error got %v", p)
-// 	} else {
-// 		t.Logf("Init(\"\") PASSED,expected <nil> got %v", p)
-// 	}
-
-// 	p, _ = Init("192.168.99.100:19092")
-// 	if p == nil {
-// 		t.Errorf("Init(\"192.168.99.100:19092\") FAILED, expected some error got %v", p)
-// 	} else {
-// 		t.Logf("Init(\"192.168.99.100:19092\") PASSED,expected <nil> got %v", p)
-// 	}
-// }
+func TestGetProducer(t *testing.T) {
+	Init("192.168.99.100:19092")
+	result := GetProducer()
+	if result != nil {
+		t.Logf("GetProducer() PASSED,expected producer got %v", result)
+	} else {
+		t.Errorf("GetMessage() FAILED,expected <nil> got %v", result)
+	}
+}
